26a69cc477fb80d6b4cb8fa73056ee3d167bce77
mezzo <mezzo@localhost>
2019-03-17 15:36:22 +0800

    debug

--- model.py
+++ model.py
@@ -33,7 +33,7 @@ class Transformer:
     def __init__(self, hp):
         self.hp = hp
         # self.token2idx, self.idx2token = load_vocab(hp.vocab)
-        self.embeddings = get_token_embeddings(self.hp.vocab_size, self.hp.d_model, zero_pad=True)
+        # self.embeddings = get_token_embeddings(self.hp.vocab_size, self.hp.d_model, zero_pad=True)
 
     def encode(self, x, training=True):
         '''
